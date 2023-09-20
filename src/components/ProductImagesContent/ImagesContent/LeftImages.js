56f4ff571555a68d49a902a6e581096323839dbd
sylee <soyeun.lee5930@gmail.com>
2023-09-20 14:22:23 +0900

    feat : product detail page imagecontent - right imagecontent

--- src/components/ProductImagesContent/ImagesContent/LeftImages.js
+++ src/components/ProductImagesContent/ImagesContent/LeftImages.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './LeftImages.scss';
 
 const LeftImages = () => {
+  const [imageData, setImageData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/imageData.json')
+      .then(Response => Response.json())
+      .then(result => setImageData(result));
+  }, []);
+
   return (
     <div className="leftImages">
       <div>
-        <div className="swiper swiper-initialized swiper-backface-hidden swiper-horizontal">
+        <div className="swiper">
           <div className="swiperWrapper">
             <div className="swiperSlide">
-              <img src="/images/boots/male/boots.jpg" alt="상품이미지" />
-              <img src="/images/boots/male/boots3.jpg" alt="상품이미지" />
-              <img src="/images/boots/male/boots4.jpg" alt="상품이미지" />
-              <img src="/images/boots/male/boots6.jpg" alt="상품이미지" />
-              <img src="/images/boots/male/boots8.jpg" alt="상품이미지" />
+              {imageData?.map(list => {
+                return (
+                  <div className="swiperSlide" key={list.alt}>
+                    <img src={list.src} alt={list.alt} />
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
--- src/components/ProductImagesContent/ImagesContent/RightImages.js
+++ src/components/ProductImagesContent/ImagesContent/RightImages.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './RightImages.scss';
 
 const RightImages = () => {
+  const [imageData, setImageData] = useState([]);
+
+  const [count, setCount] = useState(0);
+
+  const prevBtn = () => {
+    setCount(count => (count > 0 ? count - 1 : null));
+  };
+
+  const nextBtn = () => {
+    setCount(count => (count < 10 ? count + 1 : (count = 0)));
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/imageData.json')
+      .then(Response => Response.json())
+      .then(result => setImageData(result));
+  }, []);
+
   return (
     <div className="rightImages">
       <div className="swiperSlide">
-        <img src="/images/boots/male/boots.jpg" alt="상품이미지" />
-        <img src="/images/boots/male/boots3.jpg" alt="상품이미지" />
-        <img src="/images/boots/male/boots4.jpg" alt="상품이미지" />
-        <img src="/images/boots/male/boots6.jpg" alt="상품이미지" />
-        <img src="/images/boots/male/boots8.jpg" alt="상품이미지" />
+        {imageData?.map((list, index) => {
+          return (
+            <div
+              className="swiperSlider"
+              key={list.alt}
+              style={{ transform: `translateX(${count * -560}px)` }}
+            >
+              <img src={list.src} alt={list.alt} index={index} />
+            </div>
+          );
+        })}
+        <button className="prev" onClick={prevBtn}>
+          〈
+        </button>
+        <button className="next" onClick={nextBtn}>
+          〉
+        </button>
+        <div className="chapter">
+          <p>
+            {count + 1} / {imageData.length}
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default RightImages;
-
-{
-  /* <div className="rightImages">
-      <div className="swiper">
-        <div className="swiperWrapper">
-          <div className="swiperSlide">
-            <img src="/images/boots/male/boots.jpg" alt="상품이미지" />
-            <img src="/images/boots/male/boots3.jpg" alt="상품이미지" />
-            <img src="/images/boots/male/boots4.jpg" alt="상품이미지" />
-            <img src="/images/boots/male/boots6.jpg" alt="상품이미지" />
-            <img src="/images/boots/male/boots8.jpg" alt="상품이미지" />
-          </div>
-        </div>
-      </div>
-    </div> */
-}

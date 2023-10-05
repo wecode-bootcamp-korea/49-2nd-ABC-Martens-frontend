fdb2c2381b60a446a46dbf3128dad7c47f4c1e5f
sylee <soyeun.lee5930@gmail.com>
2023-10-05 19:13:38 +0900

    fix : productdetail page 목데이터 적용 및 수량 변경, 합계 금액 기능관련 구현

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ProductDetailHome from './ProductDetailHome/ProductDetailHome';
 import ProductImagesContent from './ProductImagesContent/ProductImagesContent';
 import ProductOrder from './ProductOrder/ProductOrder';
+import { HOST } from '../../components/Variable/Variable';
 import './ProductDetail.scss';
 
 const ProductDetail = () => {
+  const [productList, setProductList] = useState({
+    productId: 1,
+    description: '',
+    salePercentage: '30',
+    productName: '1461 모노',
+    images: ['imageurl', 'imageurl2', 'imageurl3'],
+    quantity: [100, 100, 100],
+    options: [230, 240, 250],
+    originalPrice: '210000',
+    price: '170000',
+  });
+  const params = useParams();
+  const id = params.id;
+
+  useEffect(() => {
+    fetch(`${HOST}/goods/detail/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  }, [id]);
+
+  if (!productList) {
+    return null;
+  }
+
   return (
     <div className="productDetail">
       <div className="layoutBody">
@@ -12,7 +44,7 @@ const ProductDetail = () => {
         <div className="productsDiv">
           <div className="productDetailContent">
             <ProductImagesContent />
-            <ProductOrder />
+            <ProductOrder productList={productList} />
           </div>
 
           <div className="reviewContent" />
